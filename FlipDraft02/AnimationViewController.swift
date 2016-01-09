ee59b212f0a14408596f0ce3b50ab4919b83627f
jhars <jjharlan06@gmail.com>
2016-01-08 22:31:03 -0800

    layout for multiple size classes in the works

--- FlipDraft02/AnimationViewController.swift
+++ FlipDraft02/AnimationViewController.swift
@@ -8,25 +8,26 @@
 
 import UIKit
 
-
-var flipView = FlipView(animationType: kAnimationFlipVertical, frame: CGRectMake(0,0,300,500))
+var flipView = FlipView(animationType: kAnimationFlipVertical, frame: CGRectMake(0,0,450,600))
 class AnimationViewController: UIViewController {
 
 
-    var animationDelegate:AnimationDelegate = AnimationDelegate(sequenceType: kSequenceTriggered, directionType: kDirectionNone)
+    var animationDelegate:AnimationDelegate = AnimationDelegate(sequenceType: kSequenceTriggered, directionType: kDirectionForward)
 //    var animationDelegate2:AnimationDelegate = AnimationDelegate(sequenceType: kSequenceTriggered, directionType: kDirectionNone)
 
     
     var step = Int()
     
+    @IBOutlet weak var NavBar: UINavigationBar!
     override func viewDidLoad() {
         super.viewDidLoad()
-            var loginView = flipView
-            loginView.center = self.view.center
-            let X_Co = ((self.view.frame.size.width - loginView.frame.width)/2)
-//            let Y_Position:CGFloat? = 500.0 //use your Y position here
-            let Y_Co = ((self.view.frame.size.height - loginView.frame.height)/2)
-            loginView.frame = CGRectMake(X_Co, Y_Co, 300, 500)
+//        var loginView = flipView
+        flipView.center = self.view.center
+//        let X_Co = ((self.view.frame.size.width - flipView.frame.width)/2)
+//        print(X_Co)
+//        let Y_Co = ((self.view.frame.size.height - flipView.frame.height)/2)
+//        print(Y_Co)
+//        flipView.frame = CGRectMake(X_Co, Y_Co - 35, self.view.frame.size.width, 600)
         
         
         animationDelegate.transformView = flipView
@@ -36,16 +37,17 @@ class AnimationViewController: UIViewController {
         flipView.font = "HelveticaNeue-Bold"
         flipView.fontSize = 24.0
         flipView.fontAlignment = "right" // not working yet... maybe when words wrap?
-        flipView.textOffset = CGPointMake(20.0, 20.0);
+        flipView.textOffset = CGPointMake(75.0, 75.0);
 //        flipView.textTruncationMode = kCATruncationEnd;
 
 
         flipView.printText("Hello-ANIMATE ", usingImage: nil, backgroundColor: UIColor.redColor(), textColor: UIColor.blueColor())
-//        flipView
         flipView.printText("Hello-REVERSE", usingImage: nil, backgroundColor: UIColor.greenColor(), textColor: UIColor.yellowColor())
         flipView.printText("START-HERE", usingImage: nil, backgroundColor: UIColor.blueColor(), textColor: UIColor.whiteColor())
         //        animationDelegate.startAnimation(kDirectionForward)
         self.view.addSubview(flipView)
+        self.view.bringSubviewToFront(self.NavBar)
+
 
         
 //        flipView.sublayerCornerRadius = 6.0
@@ -62,19 +64,19 @@ class AnimationViewController: UIViewController {
 
         animationDelegate.startAnimation(kDirectionForward)
         self.view.addSubview(flipView)
+        self.view.bringSubviewToFront(NavBar)
 //        animationDelegate.startAnimation(kDirectionForward)
     }
     
     @IBAction func ReverseButton(sender: AnyObject) {
-
-    
         animationDelegate.startAnimation(kDirectionBackward)
-        self.view.addSubview(flipView)
+        self.view.bringSubviewToFront(NavBar)
+        self.view.sendSubviewToBack(flipView)
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        animationDelegate.startAnimation(kDirectionNone)
+//        animationDelegate.startAnimation(kDirectionNone)
     }
     
     override func viewWillDisappear(animated: Bool) {
