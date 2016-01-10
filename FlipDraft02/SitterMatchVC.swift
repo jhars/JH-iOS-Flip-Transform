fc454492896fd97b145b9c57d5e4e859ee43b04b
jhars <jjharlan06@gmail.com>
2016-01-09 19:15:21 -0800

    Read Sitter Data & Update Curent User on FireBase

--- FlipDraft02/SitterMatchVC.swift
+++ FlipDraft02/SitterMatchVC.swift
@@ -11,7 +11,6 @@ import FBSDKLoginKit
 import FBSDKCoreKit
 import Firebase
 
-
 var sitterMatchModelName = [String]()
 var sitterMatchModelScore = [Int]()
 var sitterModelObjects = [SitterMatchModel]()
@@ -31,60 +30,33 @@ class SitterMatchVC: UIViewController {
     let fireBaseRef = Firebase(url: "https://sitterbookapi.firebaseio.com/users/")
     //=================================================================\\
     func returnUserData() {
-        
         let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
         let task = graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
-            
             if ((error) != nil)
             {   // Process error
                 print("Error: \(error)")
             } else {
+                
                 let userID : NSString = result.valueForKey("id") as! NSString
                 print("User ID is is: \(userID)")
                 self.currentUserId = userID as String
                 self.tempFireBaseUrlForCurrentUser = "https://sitterbookapi.firebaseio.com/users/" + (userID as String)
                 
-//                let currentUserPath = self.tempFireBaseUrlForCurrentUser
-//                let firebaseRef = Firebase(url:(currentUserPath as String) + "/sitter-list/")
-
-                self.fireBaseRef.observeEventType(.Value, withBlock: { snapshot in
-                    
-                    //Need To ADD ERROR HANDLING HERE
-                    
-//                    let sitterObjDict = snapshot.value
-                    //as! NSDictionary
-                    print(snapshot.value)
-                    print("below SitterObjDict")
-                    //=================================================================\\
-//                    let imgUrlModel = sitterObjDict["image-url"] as! String
-                    
-//                    let AlamoRef = Alamofire.request(.GET, imgUrlModel)
-//                    AlamoRef.responseImage { response in
-//                        debugPrint(response)
-//                        
-//                        print(response.request)
-//                        print(response.response)
-//                        debugPrint(response.result)
-//                        
-//                        if let image = snapshot.value {
-//                            let sitterImageModel = image as! UIImage
-//                            let sitterNameModel = sitterObjDict["name"] as! String
-//                            let sitterScoreModel = sitterObjDict["cnx-score"] as! Int
-//                            let SitterObject = SitterMatchModel(name: sitterNameModel, cnxScore: sitterScoreModel, img: UIImage(named: "bedford")!)
-//                            sitterModelObjects.append(SitterObject)
-//                            self.performSegueWithIdentifier("showSitter", sender: nil)
-//                        }
-//                        AlamoRef.resume()
-//                    }
-                })
+                let currentUserPath = self.tempFireBaseUrlForCurrentUser
+                let firebaseRef = Firebase(url:(currentUserPath as String) + "/sitter-list/")
                 
-//                firebaseRef.resume()
-
+                firebaseRef.queryOrderedByValue().observeEventType(.ChildAdded, withBlock: { snapshot in
+//                self.fireBaseRef.observeEventType(.Value, withBlock: { snapshot in
+                    //Need To ADD ERROR HANDLING HERE
+                    print(snapshot.value)
+                    //=================================================================\\
+                    //       IMAGES HERE - - - - - - - - - - } AlamoRef.resume()       \\
+                    //=================================================================\\
+                })
             } // ----- END 'else' Statement --------------//
             self.performSegueWithIdentifier("showSitter", sender: nil)
-
         }) // - - - - - - - - END Graph Request - - - - - - - - - - - - - - //
-//                            task.resume()
+
     } //============================ END  func returnUserData() ============================== //
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         let newsTVC = segue.destinationViewController as! FlipBrowseSittersVC
--- FlipDraft02/UpdateUserDataVC.swift
+++ FlipDraft02/UpdateUserDataVC.swift
@@ -7,19 +7,104 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class UpdateUserDataVC: UIViewController {
 
+    
+    @IBAction func updatBtnTapped(sender: AnyObject) {
+        var userFbookData = self.returnUserData()
+    }
+    
+    
+    @IBOutlet weak var sitterStatus: UISegmentedControl!
+    @IBOutlet weak var zipTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    // __________________________________________________________________________ //
+    
+    var sitterOrNot:Bool = false
+    var currentDate = NSDate()
+    //  FIREBASE
+    var ref = Firebase(url: "https://sitterbookapi.firebaseio.com/")
+    @IBAction func parentOrSitter(sender: UISegmentedControl) {
+        switch sitterStatus.selectedSegmentIndex{
+        case 0:
+            sitterOrNot = false
+            print("SitterStatus = False")
+        case 1:
+            print("SitterStatus = True")
+            sitterOrNot = true
+        default:
+            break;
+        }
+    }
+
+    // ______________________VIEW DID LOAD_______________________________________ //
+    // __________________________________________________________________________ //
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        zipTextField.text = "12345"
+        emailTextField.text = "test@sitterbook.com"
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func returnUserData() {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            if ((error) != nil)
+            { print("Error: \(error)")
+            } else {
+                let userID : NSString = result.valueForKey("id") as! NSString
+                print("User ID is is: \(userID)")
+                
+                let userName : NSString = result.valueForKey("name") as! NSString
+                print("User Name is: \(userName)")
+                //Create New User SignUp/Login on FireBase
+                let userEmail = self.emailTextField.text! as String
+                let userZip = self.zipTextField.text! as String
+                let userSitterStatus = self.sitterOrNot as Bool
+                let lastLogin = String(NSDate())
+                //ZIP CODE on Sign Up / Login
+                //=======Update USer INfo FireBase Ref ===========//
+                
+                let updateUserInfoUrl = "https://sitterbookapi.firebaseio.com/users/" + (userID as String)
+                var updateUserInfoRef = Firebase(url: updateUserInfoUrl)
+                
+                //_________________________________________________//
+                //FreBAse OAuth Signup & Login
+                self.ref.createUser(userEmail, password: "1111", withValueCompletionBlock:{(result) -> Void in
+                    //                    print("success sign up!"); print("User has been Created, result is \(result)")
+                    self.ref.authUser(userEmail, password:"1111", withCompletionBlock: { (authData) -> Void in
+                        // ===== DATA WRITING ROUTES =========== //
+                        updateUserInfoRef.updateChildValues(["last-login": lastLogin])
+                        updateUserInfoRef.updateChildValues(["user-name": userName])
+                        updateUserInfoRef.updateChildValues(["email": userEmail])
+                        updateUserInfoRef.updateChildValues(["sitter": userSitterStatus])
+                        updateUserInfoRef.updateChildValues(["zip": userZip])
+                        
+                    })
+                })
+            }
+        })
     }
+    // ******************* SIGN-UP ********************** //
+    func signUp(sender: AnyObject) {
+        //        if emailTextField.text == "" || passwordTextField.text == "" {
+        //            print("make sure to enter in each textfield")
+        //        } else {
+        // ===== Sign Up Logic Here ======== //
+        //        }
+    }
+    // ********************************************************** //
+
+    
+    
+
+    
+    
+    
+    
     
 
     /*
